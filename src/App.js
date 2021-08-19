bcf07ad5abebcc513f26b6c5f052ac1d3571187a
Amardeep Mourya <amardeep@agvahealthcare.com>
2021-08-19 09:06:09 +0530

    project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+import Login from './components/Login'
+import WelcomeDash from './components/WelcomeDash';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Router>
+        <Switch>
+          <Route exact path='/' component={WelcomeDash} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </Router>
+        <p>Hello react</p>
     </div>
   );
 }
---
+++ src/components/WelcomeDash.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class WelcomeDash extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Hello welcome to dashboard</h2>
+            </div>
+        )
+    }
+}
